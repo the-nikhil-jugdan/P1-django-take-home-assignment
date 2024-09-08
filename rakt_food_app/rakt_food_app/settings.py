1540343beffba26f712f4a1a6153e3948acd557b
Nikhil Jugdan <thenikhiljugdan@gmail.com>
2024-09-08 19:51:11 +0530

    Add DB Host configuration

--- rakt_food_app/rakt_food_app/settings.py
+++ rakt_food_app/rakt_food_app/settings.py
@@ -82,6 +82,7 @@ DATABASES = {
         'NAME': env.str('DB_NAME'),
         'USER': env.str('DB_USER'),
         'PASSWORD': env.str('DB_PASSWORD'),
+        'HOST': env.str('DB_HOST'),
     }
 }
 
