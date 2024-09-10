30c0d1829d36b3a3e9502be6567a605664164f2f
Nikhil Jugdan <thenikhiljugdan@gmail.com>
2024-09-10 13:42:10 +0530

    Read data into CSV

---
+++ rakt_food_app/food_trucks/management/commands/load_data.py
@@ -0,0 +1,91 @@
+from django.core.management import BaseCommand
+import pandas as pd
+from food_trucks.models import FoodTruck
+import os
+import datetime
+from django.contrib.gis.geos import Point
+from decimal import Decimal
+
+dTypes = {
+    "Address": "string",
+    "Applicant": "string",
+    "Approved": "string",
+    "ExpirationDate": "string",
+    "FacilityType": "string",
+    "Fire Prevention Districts": "string",
+    "FoodItems": "string",
+    "Latitude": "string",
+    "Location": "string",
+    "LocationDescription": "string",
+    "Longitude": "string",
+    "NOISent": "string",
+    "Neighborhoods (old)": "string",
+    "Police Districts": "string",
+    "PriorPermit": "int32",
+    "Received": "string",
+    "Schedule": "string",
+    "Status": "string",
+    "Supervisor Districts": "string",
+    "X": "float64",
+    "Y": "float64",
+    "Zip Codes": "string",
+    "block": "string",
+    "blocklot": "string",
+    "cnn": "string",
+    "dayshours": "string",
+    "locationid": "int64",
+    "lot": "string",
+    "permit": "string",
+}
+
+
+class Command(BaseCommand):
+    help = "Load data from the given file"
+
+    def add_arguments(self, parser):
+        parser.add_argument("file", type=str)
+
+    def handle(self, *args, **kwargs):
+        file = kwargs["file"]
+        data = pd.read_csv(f"{os.getcwd()}/{file}", dtype=dTypes)
+        for index, row in data.iterrows():
+            FoodTruck.objects.update_or_create(
+                location_id=row["locationid"],
+                defaults={
+                    "name": row["Applicant"],
+                    "facility_type": row["FacilityType"],
+                    "cnn": row["cnn"],
+                    "description": row["LocationDescription"],
+                    "address": row["Address"],
+                    "block_lot": row["blocklot"],
+                    "block": row["block"],
+                    "lot": row["lot"],
+                    "permit": row["permit"],
+                    "status": row["Status"],
+                    "food_items": row["FoodItems"],
+                    "schedule": row["Schedule"],
+                    "days_hours": row["dayshours"],
+                    "prior_permit": row["PriorPermit"],
+                    "fire_prevention_district": row["Fire Prevention Districts"],
+                    "police_district": row["Police Districts"],
+                    "supervisor_district": row["Supervisor Districts"],
+                    "zip_code": row["Zip Codes"],
+                    "neighborhood": row["Neighborhoods (old)"],
+                    "location": Point(
+                        (Decimal(row["Latitude"]), Decimal(row["Longitude"])), srid=4326
+                    ),
+                    # "x_coordinate": row["X"],
+                    # "y_coordinate": row["Y"],
+                },
+                create_defaults={
+                    "x_coordinate": 0,
+                    "y_coordinate": 0,
+                    "approved_date": datetime.datetime.now(),
+                    "received_date": datetime.datetime.now(),
+                    "expiration_date": datetime.datetime.now(),
+                    "location": Point(
+                        (Decimal(row["Latitude"]), Decimal(row["Longitude"])), srid=4326
+                    ),
+                },
+                # TODO: Improve Data reading into DB
+            )
--- rakt_food_app/food_trucks/models.py
+++ rakt_food_app/food_trucks/models.py
@@ -4,31 +4,31 @@ from .choices import StatusChoices
 
 # Create your models here.
 class FoodTruck(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=100)
     facility_type = models.CharField(max_length=50)
 
     cnn = models.CharField(max_length=20)
     description = models.TextField()
-    address = models.CharField(max_length=100)
+    address = models.TextField()
     block_lot = models.CharField(max_length=20)
     block = models.CharField(max_length=20)
     lot = models.CharField(max_length=20)
     permit = models.CharField(max_length=20)
     status = models.CharField(max_length=20, choices=StatusChoices.choices)
-    food_items = models.CharField(max_length=100)
-    x_coordinate = models.DecimalField(max_digits=20, decimal_places=10)
-    y_coordinate = models.DecimalField(max_digits=20, decimal_places=10)
+    food_items = models.TextField()
+    x_coordinate = models.DecimalField(max_digits=30, decimal_places=15)
+    y_coordinate = models.DecimalField(max_digits=30, decimal_places=15)
 
-    schedule = models.URLField(max_length=200)
+    schedule = models.URLField(max_length=500)
     days_hours = models.CharField(max_length=100)
     approved_date = models.DateField()
     received_date = models.DateField()
     expiration_date = models.DateField()
 
-    prior_permit = models.IntegerField()
+    prior_permit = models.IntegerField(null=True)
 
     location = models.PointField()
-    location_id = models.BigIntegerField()
+    location_id = models.BigIntegerField(unique=True)
 
     fire_prevention_district = models.CharField(max_length=6)
     police_district = models.CharField(max_length=6)
